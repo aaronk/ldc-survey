9491bcc6245d2f952eaf41ebbe3b23f438319d61
= <=>
2017-03-04 13:35:55 -0500

    works

---
+++ ldc/survey/static/survey/js/survey.js
@@ -0,0 +1,36 @@
+(function($){
+    $(document).ready(function(){
+
+        function handleDrop(event, ui){
+            ui.draggable.removeClass('ui-draggable');
+            ui.draggable.removeAttr("style")
+            $(this).children("table").append(ui.draggable);
+        }
+
+        $(".talent").draggable({
+            cursor: 'move',
+            snap: '.grid-box',
+            snapMode: 'inner',
+            revert: "invalid"
+        });
+        $(".grid-box").droppable({
+            drop: handleDrop
+        });
+
+        $(".tooltip").tooltip({
+            position: { my: "bottom", at: "top+30"}
+        });
+
+        $("#submit-button").click(function(evt){
+            // Collect from the boxes into the inputs
+            $.each(['learn','mentor','inapinch','forgetit'],function(i,val){
+                var talents = [];
+                $("#" + val).find(".talent").each(function(){
+                    talents.push($(this).attr("id"));
+                });
+                $("input[name='" + val + "']").val(talents.join());
+                console.log(talents);
+            });
+        });
+    });
+})(jQuery)
---
+++ ldc/survey/views.py
@@ -0,0 +1,43 @@
+'''
+@author: Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
+@copyright: 2017. All rights reserved.
+@license: GPL v2.0
+'''
+import traceback
+from django.shortcuts import render
+from collections import OrderedDict
+from django.core.mail import send_mail
+
+import logging
+logger = logging.getLogger('ldc-survey')
+
+titles = OrderedDict()
+titles['learn']     = 'Learn (high interest, low expertise)'
+titles['mentor']    = 'Mentor (high interest, high expertise)'
+titles['forgetit']  = 'Forget it (low interest, low expertise)'
+titles['inapinch']  = 'In a pinch (low interest, high expertise)'
+titles['name']      = 'Name'
+titles['email']     = 'Email'
+titles['involved']  = 'Would you like to be more involved?'
+
+
+def survey(request):
+    '''
+    The actual survey
+    '''
+    if request.method == 'POST':
+        try:
+            lines = []
+            for k,v in titles.iteritems():
+                lines.append(v)
+                lines.append(request.POST.get(k,''))
+                lines.append('')
+
+            # logger.info('\n'.join(lines))
+            send_mail('stuff','\n'.join(lines),'aaron.kitzmiller@gmail.com',['aaron.kitzmiller@gmail.com'])
+            return render(request,'survey/thanks.html')
+        except Exception as e:
+            logger.error('Error processing survey: %s\n%s' % (str(e),traceback.format_exc()))
+            return render(request,'survey/survey.html',{'error_message' : 'Did not work.'})
+    else:
+        return render(request,'survey/survey.html')
