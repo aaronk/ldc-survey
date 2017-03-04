d0429462b1a77066589c574b8c09a7c4125bd3f6
= <=>
2017-03-04 14:18:19 -0500

    works

--- ldc/wsgi.py
+++ ldc/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ldc.settings")
 
 application = get_wsgi_application()
+
+path = '/n/sw/www/portal'
+if path not in sys.path:
+    sys.path.append(path)
